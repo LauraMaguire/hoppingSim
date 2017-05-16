5155ce4d3c308b07340ce0925fa9e16f84e51781
Laura Maguire <lama9111@colorado.edu>
2017-05-16 15:03:16 -0600

    Finished moving example files to example directory.

---
+++ example/src/initParamExampleTmpl.m
@@ -0,0 +1,25 @@
+% Initialize parameters here and save it to a Params.mat
+% This is the tracked copy of the params. this should not be
+% edited unless you are adding a new parameter. The parameter
+% file that is called, initparams_bindobs, should be a copy of this.
+% initparams_bindobs should not be tracked.
+
+% set params
+param.numTr = 10; % number of trials
+param.trID = 1; % trial ID
+param.a = [ 1 2 3 ]; % parameter 1 that changes
+param.b = [ 4 5 ];  % parameter 2 that c
+param.c = 4; % parameter that does not change
+param.d = 5; % parameter that does not change
+
+% Fix things just in case
+if param.c > 3 
+  param.c = 2;
+  fprintf('param.c too large! Setting to it to 2\n');
+end
+% calculated parameter
+param.e = param.c / 2;
+
+% Save it
+% save( filename, variables );
+save('Params', 'param');
