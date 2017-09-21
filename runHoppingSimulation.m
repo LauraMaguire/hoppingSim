2a97b6bb498e2fb2eb39fc961c324e5f13876294
Michael Stefferson <mstefferson@gmail.com>
2017-09-21 16:19:50 -0600

    random computeMSD

--- runHoppingSimulation.m
+++ runHoppingSimulation.m
@@ -215,7 +215,7 @@ try
     msd = zeros(paramTemp.runs, numrec-1,3);
     % Call the MSD computer.
     for i=1:paramTemp.runs
-      [msdTemp,dtime] = computeMSD(xx(i,:,:), paramTemp.maxComputeMsdPnts, 0, 1);
+      [msdTemp,dtime] = computeMSD(xx(i,:,:), paramTemp.maxComputeMsdPnts, 0, 2);
       msd(i,:,:) = msdTemp;
     end
     
--- src/computeMSD.m
+++ src/computeMSD.m
@@ -1,5 +1,8 @@
-function [msd,dtime]=computeMSD(x, maxpts_msd, quadFlag, useStart)
+function [msd,dtime]=computeMSD(x, maxpts_msd, quadFlag, startId)
 %takes array x (mxdxn array), t (1xn),
+% startId = 1 use start
+% startId = 2 use rand
+% startId = 3 use end
 % calculates mean-squared and quartic displacements vs dt
 % m is number of particles
 % d is dimension, will work on any dimension vector
@@ -21,35 +24,45 @@ else
   msd=zeros(number_delta_t,3); %Store [mean, std, n]
 end
 
-parfor dt = 1:number_delta_t
-  % Make sure we have no otherlapping time windows
-  NwMax = ceil( number_timepnts / dt ) - 1;
-  if useStart
-    nStartPoss = 1:dt:NwMax*dt;
-    randInd = randperm( NwMax, min(NwMax,maxpts_msd) );
-    index_start = nStartPoss( randInd );
-    index_end = index_start + dt;
-  else
-    nEndPoss = number_timepnts : -dt : 1 + dt;
-    randInd = randperm( NwMax, min(NwMax,maxpts_msd) );
-    index_end = nEndPoss(randInd);
-    index_start = index_end - dt;
-  end
-    
-  delta_coords = x(:,:, index_end) - x(:,:,index_start);
-  % calculate displacement ^ 2
-  squared_dis = sum(delta_coords.^2,2); % dx^2+dy^2+...
-  % calculate displacement ^ 4 if flag
-  if quadFlag
-    quartic_dis = sum(delta_coords.^4,2); % dx^4+dy^4+..
-    msd(dt,:) = [mean(squared_dis(:)); ... % average
-      std(squared_dis(:)); ...; % std
-      length(squared_dis(:)); ... % n (how many points used to compute mean)
-      mean(quartic_dis(:)); ... %average
-      std(quartic_dis(:))]'; %std
-  else
-    msd(dt,:) = [mean(squared_dis(:)); ... % average
-      std(squared_dis(:)); ...; % std
-      length(squared_dis(:)) ]';
+for dt = 1:number_delta_t
+  try
+    % Make sure we have no otherlapping time windows
+    NwMax = ceil( number_timepnts / dt ) - 1;
+    if startId == 1
+      nStartPoss = 1:dt:NwMax*dt;
+      randInd = randperm( NwMax, min(NwMax,maxpts_msd) );
+      index_start = nStartPoss( randInd );
+      index_end = index_start + dt;
+    elseif startId == 2
+      randOriginShift = randperm( min(dt,number_timepnts-dt), 1 ) - 1;
+      maxStart = max( number_timepnts - dt - randOriginShift, 1);
+      nStartPoss = randOriginShift + (1:dt:maxStart);
+      randInd = randperm( length(nStartPoss), min(length(nStartPoss),maxpts_msd) );
+      index_start = nStartPoss( randInd );
+      index_end = index_start + dt;
+    else
+      nEndPoss = number_timepnts : -dt : 1 + dt;
+      randInd = randperm( NwMax, min(NwMax,maxpts_msd) );
+      index_end = nEndPoss(randInd);
+      index_start = index_end - dt;
+    end
+    delta_coords = x(:,:, index_end) - x(:,:,index_start);
+    % calculate displacement ^ 2
+    squared_dis = sum(delta_coords.^2,2); % dx^2+dy^2+...
+    % calculate displacement ^ 4 if flag
+    if quadFlag
+      quartic_dis = sum(delta_coords.^4,2); % dx^4+dy^4+..
+      msd(dt,:) = [mean(squared_dis(:)); ... % average
+        std(squared_dis(:)); ...; % std
+        length(squared_dis(:)); ... % n (how many points used to compute mean)
+        mean(quartic_dis(:)); ... %average
+        std(quartic_dis(:))]'; %std
+    else
+      msd(dt,:) = [mean(squared_dis(:)); ... % average
+        std(squared_dis(:)); ...; % std
+        length(squared_dis(:)) ]';
+    end
+  catch
+    keyboard
   end
 end
