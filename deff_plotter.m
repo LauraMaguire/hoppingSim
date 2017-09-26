3dbdc1e172e4ac2ef2632133a1f06ad70215001b
Laura Maguire <laura.maguire@colorado.edu>
2017-09-26 11:04:01 -0600

    Updated deff_plotter script

--- deff_plotter.m
+++ deff_plotter.m
@@ -14,16 +14,19 @@ for i=1:length(hopValues)
     leg{i} = num2str(hopValues(i));
     errorbar(r.kd(hop{i}),r.dPost(hop{i}),r.dErr(hop{i}),'o');
 end
-l = legend(leg,'Location','southwest');
-v = get(l,'title');
-set(v,'String','kHop')
+leg{i+1} = 'Tether Model';
+
 xlabel('KD (uM)');
 ylabel('Deff / Dfree');
 
+lc = 100;
 xx = logspace(-2,1);
-y = (1.*xx.*1.*100)./(3.*1+xx.*1.*100);
-semilogx(xx,y);
+y = (1.*(xx*1e-3).*1.*lc)./(3.*1+(xx*1e-3).*1.*lc);
+plot(xx,y,'b-');
 
+l = legend(leg,'Location','southwest');
+v = get(l,'title');
+set(v,'String','kHop')
 %%
 figure
 %h = errorbar(r.kd,r.dPost,r.dErr, 'ko');
@@ -38,13 +41,14 @@ for i=1:length(hopValues)
     leg{i} = num2str(hopValues(i));
     errorbar(r.koff(hop{i}),r.dBound(hop{i}),r.dErr(hop{i}),'o');
 end
-leg{i+1} = 'Analytic Model';
+leg{i+1} = 'Tether Model';
 xlabel('koff (us^-1)');
 ylabel('Dbound / Dfree');
 
 xx = logspace(-5,-2);
-y = (1.*xx.*1.*500)./(3.*1+xx.*1.*500);
-semilogx(xx,y);
+lc = 100;
+y = (1.*xx.*1.*lc)./(3.*1+xx.*1.*lc);
+semilogx(xx,y,'b-');
 
 l = legend(leg,'Location','southeast');
 v = get(l,'title');
@@ -64,25 +68,25 @@ for i=1:length(hopValues)
     hop{i} = find(r.khop == hopValues(i));
     leg{i} = num2str(hopValues(i));
     sel = r.bindFlux./r.nonbindFlux;
-    errorbar(r.koff(hop{i}),sel(hop{i}),r.dErr(hop{i}),'o');
+    errorbar(r.kd(hop{i}),sel(hop{i}),r.dErr(hop{i}),'o');
 end
 
-semilogx(xx,y./nb);
+semilogx(xx/1e-3,y./nb);
 l = legend(leg,'Location','southwest');
 v = get(l,'title');
 set(v,'String','kHop')
-xlabel('koff (us^-1)');
+xlabel('KD (uM)');
 ylabel('Selectivity');
 
 
 %%
-
+clear y
 xx=logspace(-5,-1,30);
 p = struct();
 p.AB = 20;
 p.L = 100;
 p.Nt = 1e3;
-p.ll=500;
+p.ll=100;
 p.DF = 1;
 p.kon = 1e-3;
 
