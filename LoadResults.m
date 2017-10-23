4b05d97a53667539284c70c289b6aefb0e02f9ab
Laura Maguire <laura.maguire@colorado.edu>
2017-10-23 11:43:59 -0600

    Adding Mike's horizontal asymptote and diffusion coefficient finders.  Updating filnames to use sci.notation for khop.

--- LoadResults.m
+++ LoadResults.m
@@ -1,4 +1,4 @@
-function r = LoadResults(plotFlag)
+function r = LoadResults()
 % Loads all output .mat files from current folder.
 % Input: plotFlag - if 1, plots showing how effective diffusion coefficient
 % was calculated will remain after runnning.  If zeros, all plots will be
@@ -11,17 +11,17 @@ d=dir('*.mat'); % list all .mat files in current folder
 l = length(d); % count all files
 r = struct(); % initialize results structure
 
-r.d = cell(1,l); % cell array to hold Deff vectors
-r.msd = cell(1,l); % cell array to hold msd vectors
-r.derr = cell(1,l); % cell array to hold error in Deff
-r.dtime = cell(1,l); % cell array to hold time vectors
-r.msderr = cell(1,l);
+r.d = cell(l,1); % cell array to hold Deff vectors
+r.msd = cell(l,1); % cell array to hold msd vectors
+r.derr = cell(l,1); % cell array to hold error in Deff
+r.dtime = cell(l,1); % cell array to hold time vectors
+r.errMean = cell(l,1);
 
-r.filename = cell(1,l); % cell array to hold file names
-r.dt = zeros(1,l); % for delta T parameter (size of timestep)
+r.filename = cell(l,1); % cell array to hold file names
+r.deltat = zeros(1,l); % for delta T parameter (size of timestep)
 r.kon = zeros(1,l); % for kon calculated after running
 r.koff = zeros(1,l); % for koff calculated after running
-r.khop = zeros(1,l); % for hopping rate parameter
+r.hopParam = zeros(1,l); % for hopping rate parameter
 r.dPre = zeros(1,l); % for predicted Deff using koff and kon above
 r.kd = zeros(1,l); % for calculated Kd (from koff and kon above)
 r.dPost = zeros(1,l); % for Deff calculated from simulation
@@ -29,6 +29,7 @@ r.dErr = zeros(1,l); % for error in calculated Deff
 r.db = zeros(1,l);
 r.hopFreq = zeros(1,l);
 r.hopOverageFreq = zeros(1,l); % for hopping overage frequency
+r.khop = zeros(1,l);
 r.onOverageCount = cell(1,l); % for on-rate overage count
 
 r.params = cell(1,l); % for parameters needed for flux calculation
@@ -37,24 +38,43 @@ for k=1:l
     fname=d(k).name;
     data=load(fname);
     r.filename{k} = fname;
-    r.d{k} = data.results.Deff;
-    r.derr{k} = data.results.Derr;
-    r.msd{k} = data.results.meanMSD;
-    r.dtime{k} = data.results.dtime;
-    r.msderr{k} = data.results.meanErr;
-    r.dt(k) = data.paramOut.deltaT;
+    if length(data.results.dtime) > 10^5
+        r.dtime{k} = data.results.dtime(1:1e5);
+        r.d{k} = data.results.Deff(1:1e5);
+        r.derr{k} = data.results.Derr(1:1e5);
+        r.msd{k} = data.results.meanMSD(1:1e5);
+        r.errMean{k} = data.results.meanErr(1:1e5);
+    elseif data.paramOut.kHop > 0
+        r.dtime{k} = data.results.dtime(1:end/10);
+        r.d{k} = data.results.Deff(1:end/10);
+        r.derr{k} = data.results.Derr(1:end/10);
+        r.msd{k} = data.results.meanMSD(1:end/10);
+        r.errMean{k} = data.results.meanErr(1:end/10);
+    else
+        r.dtime{k} = data.results.dtime;
+        r.d{k} = data.results.Deff;
+        r.derr{k} = data.results.Derr;
+        r.msd{k} = data.results.meanMSD;
+        r.errMean{k} = data.results.meanErr;
+    end
+    
+    r.deltat(k) = data.paramOut.deltaT;
     r.kon(k) = data.results.konCalc;
     r.koff(k) = data.results.koffCalc;
-    r.khop(k) = data.paramOut.kHop;
+    r.hopParam(k) = data.paramOut.kHop;
     r.dPre(k) = data.DeffCalc;
     r.kd(k) = r.koff(k)/r.kon(k);
-    [r.dPost(k), r.dErr(k)] = estimateDeff(r.dt(k)*1:length(r.d{k}),r.d{k});
+    %[r.dPost(k), r.dErr(k)] = estimateDeff(r.dt(k)*1:length(r.d{k}),r.d{k});
+    [diffInfo] = getDfromMsdData( r.dtime{k}, r.msd{k}, r.errMean{k}, 0.1, 15, 1 );
+    r.dPost(k) = diffInfo.D;
+    r.dErr(k) = diffInfo.stdD;
     
     if isfield(data.results,'hopFreq')
         r.hopFreq(k) = data.results.hopFreq;
     else
         r.hopFreq(k) = NaN;
     end
+    r.khop(k) = r.hopFreq(k)./r.deltat(k);
     
     if isfield(data.results,'hopOverageFreq')
         r.hopOverageFreq(k) = data.results.hopOverageFreq;
@@ -83,8 +103,6 @@ for k=1:l
     r.params{k}.ll = data.paramOut.lc*data.paramOut.lp;
     r.params{k}.DB = r.dBound(k);
 
-if plotFlag == 0
-    close all
 end
 
 end
--- deff_plotter.m
+++ deff_plotter.m
@@ -7,23 +7,23 @@ set(gca, 'XScale', 'log')
 %set(gca, 'YScale', 'log')
 hold all
 
-hopValues = unique(r.khop);
+hopValues = unique(r.hopParam);
 hop = cell(1,length(hopValues));
 for i=1:length(hopValues)
-    hop{i} = find(r.khop == hopValues(i));
+    hop{i} = find(r.hopParam == hopValues(i));
     leg{i} = num2str(hopValues(i));
     errorbar(r.kd(hop{i}),r.dPost(hop{i}),r.dErr(hop{i}),'o','MarkerSize',10);
 end
 leg{i+1} = 'Tether Model';
 
 xlabel('KD (uM)');
-ylabel('Deff / Dfree');
+ylabel('Dbound / Dfree');
 
-lc = 500;
+lc = 100;
 xx = logspace(-3,1);
 y = (1.*(xx*1e-3).*1.*lc)./(3.*1+(xx*1e-3).*1.*lc);
 plot(xx,y,'b-');
-axis([9e-4 1e1 0 1]);
+%axis([9e-4 1e1 0 1]);
 l = legend(leg,'Location','southwest');
 v = get(l,'title');
 set(v,'String','kHop')
@@ -62,17 +62,18 @@ set(gca, 'XScale', 'log')
 %set(gca, 'YScale', 'log')
 hold all
 
-hopValues = unique(r.khop);
+hopValues = unique(r.hopParam);
 hop = cell(1,length(hopValues));
 for i=1:length(hopValues)
-    hop{i} = find(r.khop == hopValues(i));
+    hop{i} = find(r.hopParam == hopValues(i));
     leg{i} = num2str(hopValues(i));
     sel = r.bindFlux./r.nonbindFlux;
     err = r.bFluxErr./r.nonbindFlux;
-    errorbar(r.kd(hop{i}),sel(hop{i}),err(hop{i}),'o','MarkerSize',10);
+    %errorbar(r.kd(hop{i}),sel(hop{i}),err(hop{i}),'o','MarkerSize',10);
+    plot(r.kd(hop{i}),sel(hop{i}),'o','MarkerSize',10);
 end
 
-axis([1e-3 1e2 0 80]);
+axis([5e-2 1e2 0 100]);
 semilogx(xx/1e-3,y./nb,'b-');
 l = legend(leg,'Location','southwest');
 v = get(l,'title');
@@ -88,7 +89,7 @@ p = struct();
 p.AB = 20;
 p.L = 100;
 p.Nt = 1e3;
-p.ll=500;
+p.ll=100;
 p.DF = 1;
 p.kon = 1e-3;
 
@@ -97,4 +98,12 @@ for j=1:length(xx)
     p.koff = xx(j);
     disp(num2str(j));
     [y(j), nb(j)] = subNum(p,1);
+end
+
+%%
+onOver = 0;
+for k = 1:length(r.kd)
+    ontemp = sum(r.onOverageCount{k});
+    disp(['Exp. ' num2str(k) ', onOverage = ' num2str(ontemp)]);
+    onOver = onOver + ontemp;
 end
